bd810665de092f82ec354650ee57cd2f817685e0
kastiel404 <kastiel404@gmail.com>
2019-05-25 18:37:01 +0200

    Add MealServlet with mapping

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -3,21 +3,17 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 public class UserServlet extends HttpServlet {
     private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         LOG.debug("redirect to users");
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
     }
 }
