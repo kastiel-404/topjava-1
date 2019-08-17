f4265e5ba9f0903d3ecce554813457c4317eb7b1
kastiel404 <kastiel404@gmail.com>
2019-08-17 19:48:23 +0200

    add timing rules

---
+++ src/main/java/ru/javawebinar/topjava/TimingRules.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.topjava;
+
+import org.junit.runner.Description;
+import org.junit.Rule;
+import org.junit.rules.ExternalResource;
+import org.slf4j.Logger;
+import org.junit.rules.Stopwatch;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class TimingRules {
+    private static final Logger log = getLogger("result");
+
+    private static StringBuilder results = new StringBuilder();
+
+
+    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
+
+    public static final Stopwatch STOPWATCH = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String result = String.format("\n%-25s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+            results.append(result).append('\n');
+            log.info(result + " ms\n");
+        }
+    };
+
+    public static final ExternalResource SUMMARY = new ExternalResource() {
+        @Override
+        protected void before() throws Throwable {
+            results.setLength(0);
+        }
+
+        @Override
+        protected void after() {
+            log.info("\n-------------------------------------------------------------------------------------------------------" +
+            "\nTest                                                                                       Duration, ms" +
+            "\n-------------------------------------------------------------------------------------------------------\n" +
+            results +
+                        "-------------------------------------------------------------------------------------------------------\n");
+        }
+    };
+}
